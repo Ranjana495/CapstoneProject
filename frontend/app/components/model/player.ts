539f01f9da6704b0c9669390b654e3f8e5bfe049
Ranjana495 <71600700+Ranjana495@users.noreply.github.com>
2020-09-20 22:42:03 +0530

    Add files via upload

---
+++ frontend/app/components/model/player.ts
@@ -0,0 +1,7 @@
+export class Player {
+    id: number;
+    playername: string;
+    age: number;
+    caption: string;
+    playersteam: string
+}
---
+++ frontend/app/components/model/score.ts
@@ -0,0 +1,5 @@
+export class Score {
+    sid: number;
+    name: string;
+    scoreval:number;
+}

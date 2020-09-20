7e1b5bc123cc884fd58735d4150fd2678c20d4fa
Ranjana495 <71600700+Ranjana495@users.noreply.github.com>
2020-09-20 22:11:44 +0530

    Add files via upload

---
+++ backendCode/data.sql
@@ -0,0 +1,70 @@
+insert into team(teamname)
+values('Charlie');
+insert into team(teamname)
+values('Chaplin');
+insert into team(teamname)
+values('Royal Challengers');
+insert into team(teamname)
+values('Champions');
+insert into team(teamname)
+values('Bosses');
+insert into team(teamname)
+values('Amigos');
+
+
+insert into player(pid,playername,age,caption,playersteam,score)
+values(10,'Ranga',19,null,'Charlie',0);
+insert into player(pid,playername,age,caption,playersteam,score)
+values(11,'Ranja',33,null,'Charlie',0);
+insert into player(pid,playername,age,caption,playersteam,score)
+values(12,'ranjana',25,null,'Charlie',0);
+insert into player(pid,playername,age,caption,playersteam,score)
+values(22,'Gulal',26,null,'Charlie',0);
+insert into player(pid,playername,age,caption,playersteam,score)
+values(23,'Alice',29,null,'Charlie',0);
+
+insert into player(pid,playername,age,caption,playersteam,score)
+values(13,'Arav',18,null,'Chaplin',0);
+insert into player(pid,playername,age,caption,playersteam,score)
+values(14,'Arun',30,null,'Chaplin',0);
+insert into player(pid,playername,age,caption,playersteam,score)
+values(15,'Sawastik',25,null,'Chaplin',0);
+
+insert into player(pid,playername,age,caption,playersteam,score)
+values(16,'Pandaya',20,null,'Royal Challengers',0);
+insert into player(pid,playername,age,caption,playersteam,score)
+values(17,'Sachin',34,null,'Royal Challengers',0);
+insert into player(pid,playername,age,caption,playersteam,score)
+values(18,'Taruna',26,null,'Royal Challengers',0);
+
+insert into player(pid,playername,age,caption,playersteam,score)
+values(19,'Rohan',24,null,'Champions',0);
+insert into player(pid,playername,age,caption,playersteam,score)
+values(20,'Anshu',36,null,'Champions',0);
+insert into player(pid,playername,age,caption,playersteam,score)
+values(21,'Chhaya',35,null,'Champions',0);
+
+
+insert into player(pid,playername,age,caption,playersteam,score)
+values(24,'veer',32,null,'Bosses',0);
+insert into player(pid,playername,age,caption,playersteam,score)
+values(25,'Don',32,null,'Bosses',0);
+insert into player(pid,playername,age,caption,playersteam,score)
+values(26,'Sawati',25,null,'Bosses',0);
+
+insert into player(pid,playername,age,caption,playersteam,score)
+values(27,'Bob',29,null,'Amigos',0);
+insert into player(pid,playername,age,caption,playersteam,score)
+values(28,'kuhu',24,null,'Amigos',0);
+insert into player(pid,playername,age,caption,playersteam,score)
+values(29,'Tarun',27,null,'Amigos',0);
+
+
+insert into scores(sid,name,scoreval)
+values(1,'Ranga',22);
+insert into scores(sid,name,scoreval)
+values(2,'niga',32);
+insert into scores(sid,name,scoreval)
+values(3,'saga',12);
+
+
